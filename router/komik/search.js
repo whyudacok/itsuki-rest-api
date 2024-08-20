6efa63e47f860f2caa25f454dfa3d23d0264b318
Whyu Gemink <119993190+whyudacok@users.noreply.github.com>
2024-08-21 05:03:12 +0700

    Update search.js

--- router/komik/search.js
+++ router/komik/search.js
@@ -1,18 +1,13 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
-const NodeCache = require('node-cache');
 const router = express.Router();
 const { baseUrl } = require('../base-url');
-
-// Inisialisasi cache dengan TTL 10 menit
-const cache = new NodeCache({ stdTTL: 600, checkperiod: 120 });
 
 router.get('/:page', async (req, res) => {
     const { page } = req.params;
     const { genre = [], status, type, format, order, project, title } = req.query;
 
-    // Genre bisa menjadi array, jadi kita harus memprosesnya sebagai array
     let genreQuery = '';
     if (Array.isArray(genre)) {
         genreQuery = genre.map((g, index) => `genre%5B${index}%5D=${g}`).join('&');
@@ -20,8 +15,7 @@ router.get('/:page', async (req, res) => {
         genreQuery = `genre%5B0%5D=${genre}`;
     }
 
-    // Helper function untuk membangun query string
-    const buildQuery = (key, value) => (value !== undefined && value !== '') ? `${key}=${value}` : key;
+    const buildQuery = (key, value) => (value !== undefined && value !== '') ? `${key}=${value}` : '';
 
     const queryString = [
         genreQuery,
@@ -32,14 +26,6 @@ router.get('/:page', async (req, res) => {
         buildQuery('project', project),
         buildQuery('title', title)
     ].filter(Boolean).join('&');
-
-    const cacheKey = `komik-page-${page}?${queryString}`;
-    const cachedData = cache.get(cacheKey);
-
-    if (cachedData) {
-        // Jika data ditemukan di cache, kirim data dari cache
-        return res.json({ success: true, data: cachedData });
-    }
 
     const url = `${baseUrl}/daftar-komik/page/${page}/?${queryString}`;
 
@@ -57,7 +43,6 @@ router.get('/:page', async (req, res) => {
         const html = response.data;
         const $ = cheerio.load(html);
 
-        // Update semua href yang diawali dengan base URL
         $(`a[href^="${baseUrl}"]`).each((_, el) => {
             const href = $(el).attr('href');
             $(el).attr('href', href.replace(baseUrl, ''));
@@ -92,12 +77,9 @@ router.get('/:page', async (req, res) => {
             }
         };
 
-        // Simpan data ke cache
-        cache.set(cacheKey, responseData.data);
-
-        // Kirim respons JSON ke client
         res.json(responseData);
     } catch (error) {
+        console.error(`Error fetching data from ${url}:`, error.message);
         res.status(500).json({ success: false, message: 'Terjadi kesalahan saat mengambil data.' });
     }
 });
