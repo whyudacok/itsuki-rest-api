131f1a9565cf8a78ce389b0445e7f07fbca5d0fc
Whyu Gemink <119993190+whyudacok@users.noreply.github.com>
2024-08-21 04:49:44 +0700

    Update cari.js

--- router/komik/cari.js
+++ router/komik/cari.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
-const NodeCache = require('node-cache');
 const router = express.Router();
 const { baseUrl } = require('../base-url');
-
-// Inisialisasi cache dengan TTL 10 menit
-const cache = new NodeCache({ stdTTL: 600 });
 
 router.get('/:endpoint/:page', async (req, res) => {
   const { endpoint, page } = req.params;
   const url = `${baseUrl}/page/${page}/?s=${encodeURIComponent(endpoint)}`;
-
-  // Cek apakah data sudah ada di cache
-  const cachedData = cache.get(url);
-  if (cachedData) {
-    return res.json(cachedData);
-  }
 
   try {
     const response = await axios.get(url, {
@@ -32,28 +22,15 @@ router.get('/:endpoint/:page', async (req, res) => {
     const html = response.data;
     const $ = cheerio.load(html);
 
-    // Update semua href yang diawali dengan base URL
-    $(`a[href^="${baseUrl}"]`).each((_, el) => {
-      const href = $(el).attr('href');
-      $(el).attr('href', href.replace(baseUrl, ''));
-    });
-
     const results = [];
     $('.post-item-box').each((_, el) => {
-      const link = $(el).find('a').attr('href');
-      const type = $(el).find('.flag-country-type').attr('class').split(' ').pop();
-      const gambar = $(el).find('.post-item-thumb img').attr('src');
-      const title = $(el).find('.post-item-title h4').text().trim();
-      const rating = $(el).find('.rating i').text().trim();
-      const warna = $(el).find('.color-label-manga').text().trim();
-
       results.push({
-        link,
-        type,
-        gambar,
-        title,
-        rating,
-        warna
+        link: $(el).find('a').attr('href'),
+        type: $(el).find('.flag-country-type').attr('class').split(' ').pop(),
+        gambar: $(el).find('.post-item-thumb img').attr('src'),
+        title: $(el).find('.post-item-title h4').text().trim(),
+        rating: $(el).find('.rating i').text().trim(),
+        warna: $(el).find('.color-label-manga').text().trim()
       });
     });
 
@@ -64,9 +41,6 @@ router.get('/:endpoint/:page', async (req, res) => {
       results,
       totalPages
     };
-
-    // Simpan data ke cache
-    cache.set(url, data);
 
     res.json(data);
   } catch (error) {
