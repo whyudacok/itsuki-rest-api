d4558781e9f83d562efe697677b4c890df80ef9e
Whyu Gemink <119993190+whyudacok@users.noreply.github.com>
2024-08-21 04:44:46 +0700

    Update update.js

--- router/komik/update.js
+++ router/komik/update.js
@@ -1,22 +1,12 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
-const NodeCache = require('node-cache');
 const router = express.Router();
 const { baseUrl } = require('../base-url');
-
-// Buat instance cache baru dengan TTL 10 menit (600 detik)
-const cache = new NodeCache({ stdTTL: 600 });
 
 router.get('/:page', async (req, res) => {
   const { page } = req.params;
   const url = `${baseUrl}/komik-terbaru/page/${page}/`;
-
-  // Cek apakah data ada di cache
-  const cachedData = cache.get(url);
-  if (cachedData) {
-    return res.json(cachedData);
-  }
 
   try {
     const response = await axios.get(url, {
@@ -32,51 +22,30 @@ router.get('/:page', async (req, res) => {
     const html = response.data;
     const $ = cheerio.load(html);
 
-    // Update semua href yang diawali dengan baseUrl
-    $(`a[href^="${baseUrl}"]`).each((_, el) => {
-      const href = $(el).attr('href');
-      $(el).attr('href', href.replace(baseUrl, ''));
-    });
-
     const updateKomik = [];
     $('.post-item-box').each((_, el) => {
-      const link = $(el).find('a').attr('href');
-      const type = $(el).find('.flag-country-type').attr('class').split(' ').pop();
-      const gambar = $(el).find('.post-item-thumb img').attr('src');
-      const Title = $(el).find('.post-item-title h4').text().trim();
-      const chapterLink = $(el).find('.lsch a').attr('href');
-      const chapterJudul = $(el).find('.lsch a').text().trim();
-      const chapterTanggal = $(el).find('.datech').text().trim();
-      const warna = $(el).find('.color-label-manga').text().trim();
-
       updateKomik.push({
-        link,
-        type,
-        gambar,
-        Title,
-        warna,
+        link: $(el).find('a').attr('href'),
+        type: $(el).find('.flag-country-type').attr('class').split(' ').pop(),
+        gambar: $(el).find('.post-item-thumb img').attr('src'),
+        Title: $(el).find('.post-item-title h4').text().trim(),
+        warna: $(el).find('.color-label-manga').text().trim(),
         chapter: {
-          link: chapterLink,
-          Title: chapterJudul,
-          Date: chapterTanggal
+          link: $(el).find('.lsch a').attr('href'),
+          Title: $(el).find('.lsch a').text().trim(),
+          Date: $(el).find('.datech').text().trim()
         }
       });
     });
 
     const komikPopuler = [];
     $('.list-series-manga.pop li').each((_, el) => {
-      const populerLink = $(el).find('.thumbnail-series a.series').attr('href');
-      const populerGambar = $(el).find('.thumbnail-series img').attr('src');
-      const peringkat = $(el).find('.ctr').text().trim();
-      const populerJudul = $(el).find('h4 a.series').text().trim();
-      const rating = $(el).find('.loveviews').text().trim();
-
       komikPopuler.push({
-        link: populerLink,
-        gambar: populerGambar,
-        peringkat,
-        Title: populerJudul,
-        rating
+        link: $(el).find('.thumbnail-series a.series').attr('href'),
+        gambar: $(el).find('.thumbnail-series img').attr('src'),
+        peringkat: $(el).find('.ctr').text().trim(),
+        Title: $(el).find('h4 a.series').text().trim(),
+        rating: $(el).find('.loveviews').text().trim()
       });
     });
 
@@ -88,9 +57,6 @@ router.get('/:page', async (req, res) => {
       Totalpages,
       komikPopuler
     };
-
-    // Simpan data ke cache
-    cache.set(url, data);
 
     // Kirim respons JSON ke client
     res.json(data);
