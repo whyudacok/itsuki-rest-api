7b973e5e5bbd887578ae34f3c8585c319090bc8e
Whyu Gemink <119993190+whyudacok@users.noreply.github.com>
2024-08-21 04:56:18 +0700

    Update genre.js

--- router/komik/genre.js
+++ router/komik/genre.js
@@ -1,22 +1,11 @@
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
-const NodeCache = require('node-cache');
 const router = express.Router();
 const { baseUrl } = require('../base-url');
 
-const cache = new NodeCache({ stdTTL: 600 }); // TTL 600 detik atau 10 menit
-
 router.get('/:genre/:page', async (req, res) => {
   const { genre, page } = req.params;
-  const cacheKey = `genre_${genre}_page_${page}`;
-
-  // Cek apakah data sudah ada di cache
-  const cachedData = cache.get(cacheKey);
-  if (cachedData) {
-    return res.json({ success: true, data: cachedData });
-  }
-
   const url = `${baseUrl}/genres/${genre}/page/${page}/`;
 
   try {
@@ -33,45 +22,27 @@ router.get('/:genre/:page', async (req, res) => {
     const html = response.data;
     const $ = cheerio.load(html);
 
-    // Update semua href yang diawali dengan base URL
-    $(`a[href^="${baseUrl}"]`).each((_, el) => {
-      const href = $(el).attr('href');
-      $(el).attr('href', href.replace(baseUrl, ''));
-    });
-
     const mangaList = [];
     $('.post-item-box').each((_, el) => {
-      const link = $(el).find('a').attr('href');
-      const jenis = $(el).find('.flag-country-type').attr('class').split(' ').pop();
-      const gambar = $(el).find('.post-item-thumb img').attr('src');
-      const judul = $(el).find('.post-item-title h4').text().trim();
-      const nilai = $(el).find('.rating i').text().trim();
-      const warna = $(el).find('.color-label-manga').text().trim();
-
       mangaList.push({
-        link,
-        jenis,
-        gambar,
-        judul,
-        nilai,
-        warna
+        link: $(el).find('a').attr('href'),
+        jenis: $(el).find('.flag-country-type').attr('class').split(' ').pop(),
+        gambar: $(el).find('.post-item-thumb img').attr('src'),
+        judul: $(el).find('.post-item-title h4').text().trim(),
+        nilai: $(el).find('.rating i').text().trim(),
+        warna: $(el).find('.color-label-manga').text().trim()
       });
     });
 
     const totalPages = parseInt($('.pagination a.page-numbers').eq(-2).text().trim());
 
-    const responseData = {
+    res.json({
       success: true,
       data: {
         mangaList,
         totalPages
       }
-    };
-
-    // Simpan data ke cache selama 10 menit
-    cache.set(cacheKey, responseData.data);
-
-    res.json(responseData);
+    });
   } catch (error) {
     res.status(500).json({ success: false, message: 'Terjadi kesalahan saat mengambil data.' });
   }
