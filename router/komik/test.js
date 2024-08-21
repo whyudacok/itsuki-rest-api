66b21330b428c1105649fbd1aca0aab740f97b4e
Whyu Gemink <119993190+whyudacok@users.noreply.github.com>
2024-08-22 02:04:28 +0700

    Update test.js

--- router/komik/test.js
+++ router/komik/test.js
@@ -8,11 +8,14 @@ router.get('/', async (req, res) => {
 
   try {
     const response = await axios.get(url, {
-      headers: {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
-      }
-    });
-
+  headers: {
+    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Accept-Language': 'en-US,en;q=0.9',
+    'Referer': 'https://www.google.com/'
+  }
+});
     const html = response.data;
     const $ = cheerio.load(html);
 
