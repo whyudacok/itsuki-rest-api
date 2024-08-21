cbb4352177b85370f05f3226fdcdc54c7c220b78
Whyu Gemink <119993190+whyudacok@users.noreply.github.com>
2024-08-22 02:18:08 +0700

    Update test.js

--- router/komik/test.js
+++ router/komik/test.js
@@ -4,30 +4,26 @@ const cheerio = require('cheerio');
 const router = express.Router();
 
 router.get('/', async (req, res) => {
-  const url = 'https://senatorpeters.com/';
+  const url = 'https://nontonfilmgratis.club/';
 
   try {
     const response = await axios.get(url, {
-  headers: {
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Referer': 'https://www.google.com/'
-  }
-});
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36'
+      }
+    });
+
     const html = response.data;
     const $ = cheerio.load(html);
 
     // Ambil elemen dengan class 'col-md-125' dan itemtype 'https://schema.org/Movie'
-    const movieDiv = $('.col-md-125[itemscope][itemtype="https://schema.org/Movie"]');
+    const movieDiv = $('.col-md-125[itemscope="itemscope"][itemtype="https://schema.org/Movie"]');
 
     // Scraping data dari elemen tersebut
     const title = movieDiv.find('h2.entry-title a').text().trim();
     const link = movieDiv.find('h2.entry-title a').attr('href');
     const imageUrl = movieDiv.find('img').attr('src');
     const rating = movieDiv.find('.gmr-rating-item').text().trim();
-    const releaseDate = movieDiv.find('time[itemprop="dateCreated"]').attr('datetime');
     const director = movieDiv.find('[itemprop="director"] [itemprop="name"]').text().trim();
     const quality = movieDiv.find('.gmr-quality-item').text().trim();
 
@@ -37,7 +33,6 @@ router.get('/', async (req, res) => {
       link,
       imageUrl,
       rating,
-      releaseDate,
       director,
       quality
     };
