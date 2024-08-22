2300891f4778affddafabaeb62b81eb4a25bf88b
Whyu Gemink <119993190+whyudacok@users.noreply.github.com>
2024-08-22 13:46:37 +0700

    Update latest.js

--- router/film/latest.js
+++ router/film/latest.js
@@ -29,7 +29,8 @@ router.get('/:page', async (req, res) => {
         gambar: $(el).find('.content-thumbnail img').attr('src'),
         rating: $(el).find('.gmr-rating-item').text().trim(),
         durasi: $(el).find('.gmr-duration-item').text().trim(),
-        quality: $(el).find('.gmr-quality-item a').text().trim()
+        quality: $(el).find('.gmr-quality-item a').text().trim(),
+        trailer: $(element).find('.gmr-popup-button a').attr('href') || null
       });
     });
 
