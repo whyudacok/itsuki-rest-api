adb4cc51079b75fdd1abfc1f385809c718deb040
whyudacok <119993190+whyudacok@users.noreply.github.com>
2024-08-27 15:26:36 +0700

    Update studio.js

--- router/anime/studio.js
+++ router/anime/studio.js
@@ -24,6 +24,18 @@ router.get('/:studio/:page?', async (req, res) => {
       const href = $(el).attr('href');
       $(el).attr('href', href.replace(aniUrl, ''));
     });
+    // Ambil semua elemen <img>
+$('img').each((_, el) => {
+    // Ambil src dari gambar
+    let src = $(el).attr('src') || '';
+
+    // Ganti bagian dari src yang sesuai
+    if (src.includes('tv.animisme.net/wp-content/uploads')) {
+        src = src.replace('tv.animisme.net/wp-content/uploads', 'animasu.cc/wp-content/uploads');
+        $(el).attr('src', src);
+    }
+});
+
 
     const result = [];
 
