e32d227a619855e72355c960f3221c45a694a7a7
whyudacok <119993190+whyudacok@users.noreply.github.com>
2024-10-08 20:34:02 +0700

    Update update.js

--- router/komik/update.js
+++ router/komik/update.js
@@ -1,3 +1,4 @@
+// routes/komikTerbaru.js
 const express = require('express');
 const axios = require('axios');
 const cheerio = require('cheerio');
@@ -9,32 +10,26 @@ router.get('/:page', async (req, res) => {
   const url = `${baseUrl}/komik-terbaru/page/${page}/`;
 
   try {
-const response = await axios.get(url, {
-  headers: {
-    'Origin': baseUrl,
-    'Referer': baseUrl,
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
-    'Accept-Encoding': 'gzip, deflate, br',
-    'Accept-Language': 'en-US,en;q=0.9',
-    'Connection': 'keep-alive',
-    'Cache-Control': 'max-age=0',
-    'Upgrade-Insecure-Requests': '1',
-    'Sec-Fetch-Dest': 'document',
-    'Sec-Fetch-Mode': 'navigate',
-    'Sec-Fetch-Site': 'same-origin',
-    'Sec-Fetch-User': '?1'
-  }
-});
-
+    const response = await axios.get(url, {
+      headers: {
+        'Origin': baseUrl,
+        'Referer': baseUrl,
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8',
+        'Accept-Encoding': 'gzip, deflate, br',
+        'Accept-Language': 'en-US,en;q=0.9',
+        'Connection': 'keep-alive',
+        'Cache-Control': 'max-age=0',
+        'Upgrade-Insecure-Requests': '1',
+        'Sec-Fetch-Dest': 'document',
+        'Sec-Fetch-Mode': 'navigate',
+        'Sec-Fetch-Site': 'same-origin',
+        'Sec-Fetch-User': '?1'
+      }
+    });
 
     const html = response.data;
     const $ = cheerio.load(html);
-
-    $(`a[href^="${baseUrl}"]`).each((_, el) => {
-      const href = $(el).attr('href');
-      $(el).attr('href', href.replace(baseUrl, ''));
-    });
 
     const latestkomik = [];
     $('.post-item-box').each((_, el) => {
@@ -43,7 +38,6 @@ const response = await axios.get(url, {
         type: $(el).find('.flag-country-type').attr('class').split(' ').pop(),
         gambar: $(el).find('.post-item-thumb img').attr('src'),
         Title: $(el).find('.post-item-title h4').text().trim(),
-        warna: $(el).find('.color-label-manga').text().trim(),
         chapter: {
           link: $(el).find('.lsch a').attr('href'),
           Title: $(el).find('.lsch a').text().trim(),
@@ -71,13 +65,11 @@ const response = await axios.get(url, {
       komikPopuler
     };
 
-    // Kirim respons JSON ke client
     res.status(200).json({
       status: true,
       data
     });
   } catch (error) {
-    // Kirim respons JSON dengan kesalahan
     res.status(500).json({
       status: false,
       data: {
