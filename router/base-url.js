b4e9779e9b2787a3076453be04c7085a4b9b82ac
whyudacok <119993190+whyudacok@users.noreply.github.com>
2024-10-08 20:57:22 +0700

    Update base-url.js

--- router/base-url.js
+++ router/base-url.js
@@ -1,5 +1,5 @@
 module.exports = {
-    baseUrl: 'https://komikcast.one',
+    baseUrl: 'https://app.scrapingbee.com/api/v1/?api_key=OQ3DTRI7YLDY3Z9Z6XD1S1V53T0F33T55XNAH0RV73J6ENM8F7G1ZPE60BPL7OZGZKOY11OF9ATYWOUQ&url=https://komikcast.one',
     aniUrl: 'https://tv.animisme.net',
     film: 'https://157.230.44.16'
 };
