b7c2aa573d9dc11ac7c467c9eab4e58667f2b0e6
whyudacok <119993190+whyudacok@users.noreply.github.com>
2024-10-09 20:07:32 +0700

    Update test.js

--- router/komik/test.js
+++ router/komik/test.js
@@ -9,13 +9,17 @@ router.get('/scrape', async (req, res) => {
     try {
         // Menggunakan User-Agent untuk menghindari error 403
         const response = await axios.get('https://komikcast.cz/', {
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Mobile Safari/537.36',
-                'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
-                'Accept-Language': 'en-US,en;q=0.5',
-                'Cache-Control': 'no-cache'
-            }
-        });
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Mobile Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+        'Accept-Language': 'en-US,en;q=0.5',
+        'Cache-Control': 'no-cache',
+        'DNT': '1', // Do Not Track Request Header
+        'Connection': 'keep-alive',
+        'Upgrade-Insecure-Requests': '1',
+    }
+});
+
 
         const html = response.data;
         const $ = cheerio.load(html);
