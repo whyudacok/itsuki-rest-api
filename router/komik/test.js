6deb35f7ad9cf92d51f0afe932df13f9381a9e92
whyudacok <119993190+whyudacok@users.noreply.github.com>
2024-10-09 18:35:33 +0700

    Update test.js

--- router/komik/test.js
+++ router/komik/test.js
@@ -9,9 +9,18 @@ router.get('/', async (req, res) => {
     const { data } = await axios.get(url, {
       headers: {
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36',
+        'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
         'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'en-US,en;q=0.9',
-        'Cache-Control': 'no-cache'
+        'Cache-Control': 'no-cache',
+      },
+      proxy: {
+        host: '189.41.83.62',
+        port: 1080,
+        auth: {
+          username: 'YOUR_PROXY_USERNAME', // jika diperlukan
+          password: 'YOUR_PROXY_PASSWORD'  // jika diperlukan
+        }
       }
     });
 
